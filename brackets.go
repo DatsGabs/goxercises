d430068330e6bc3e176bd237ffffcccd571975f2
Gabriel Lazcano <gabriel.david.lazcano@gmail.com>
2020-11-21 11:19:28 -0600

    first commit

---
+++ brackets.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+var m = map[string]string{
+	"{": "}",
+	"(": ")",
+	"[": "]",
+}
+
+// ValidBraces detect if braces are valid
+func ValidBraces(str string) bool {
+	stack := make([]string, 0)
+	for _, bracket := range str {
+		bracket := string(bracket)
+		top := len(stack) - 1
+		if len(stack) > 0 && m[stack[top]] == bracket {
+			stack = stack[:top]
+		} else {
+			stack = append(stack, bracket)
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	fmt.Println(ValidBraces("(){}"))
+}
